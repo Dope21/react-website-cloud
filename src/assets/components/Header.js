e2e08f5d4a837509d5d639499e71cc0923d055d0
Thanasak <bestzacap20@gmail.com>
2022-05-24 15:14:37 +0700

    set-up tailwind

---
+++ src/assets/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+        <nav>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
