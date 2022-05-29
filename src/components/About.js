1fa1f3b1181120f26bca40ba8df5a020fd806dab
Thanasak <bestzacap20@gmail.com>
2022-05-29 15:30:51 +0700

    update

--- src/components/About.js
+++ src/components/About.js
@@ -2,30 +2,32 @@ import React from 'react'
 
 function About() {
   return (
-    <section name="about">
-    <div className='w-full my-20'>
-        <div className='max-w-[1240px] mx-auto px-4'>
-            <div className='text-center'>
-                <h3 className='text-4xl font-bold'>Trusted by developers across the world</h3>
-                <p className='text-xl p-6 text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit non quod quia praesentium eaque fuga voluptatum a ratione maxime quasi, maiores, voluptates optio doloribus. Hic ipsam sequi cupiditate illo.</p>
+    <>
+        <section name="about">
+            <div className='w-full my-20'>
+                <div className='max-w-[1240px] mx-auto px-4'>
+                    <div className='text-center'>
+                        <h3 className='text-4xl font-bold'>Trusted by developers across the world</h3>
+                        <p className='text-xl p-6 text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit non quod quia praesentium eaque fuga voluptatum a ratione maxime quasi, maiores, voluptates optio doloribus. Hic ipsam sequi cupiditate illo.</p>
+                    </div>
+                    <div className='grid md:grid-cols-3 gap-4 md:gap-1 px-2 text-center'>
+                        <div className='border py-8 rounded-xl shadow-md md:shadow-xl'>
+                            <h4 className='text-6xl font-bold text-violet-600'>100%</h4>
+                            <p className='text-gray-400 mt-2'>Copletion</p>
+                        </div>
+                        <div className='border py-8 rounded-xl shadow-md md:shadow-xl'>
+                            <h4 className='text-6xl font-bold text-violet-600'>24/7</h4>
+                            <p className='text-gray-400 mt-2'>Delivery</p>
+                        </div>
+                        <div className='border py-8 rounded-xl shadow-md md:shadow-xl'>
+                            <h4 className='text-6xl font-bold text-violet-600'>100K</h4>
+                            <p className='text-gray-400 mt-2'>Transactions</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='grid md:grid-cols-3 gap-4 md:gap-1 px-2 text-center'>
-                <div className='border py-8 rounded-xl shadow-md md:shadow-xl'>
-                    <h4 className='text-6xl font-bold text-violet-600'>100%</h4>
-                    <p className='text-gray-400 mt-2'>Copletion</p>
-                </div>
-                <div className='border py-8 rounded-xl shadow-md md:shadow-xl'>
-                    <h4 className='text-6xl font-bold text-violet-600'>24/7</h4>
-                    <p className='text-gray-400 mt-2'>Delivery</p>
-                </div>
-                <div className='border py-8 rounded-xl shadow-md md:shadow-xl'>
-                    <h4 className='text-6xl font-bold text-violet-600'>100K</h4>
-                    <p className='text-gray-400 mt-2'>Transactions</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    </section>
+        </section>
+    </>
   )
 }
 
--- src/components/AllinOne.js
+++ src/components/AllinOne.js
@@ -3,89 +3,91 @@ import { CheckIcon } from '@heroicons/react/outline'
 
 function AllinOne() {
   return (
-    <section name="all-in-one">
-        <div className='w-full my-20'>
-            <div className='max-w-[1240px] mx-auto px-4'>
-                <h3 className='text-4xl font-bold text-center'>All-In-One Platform</h3>
-                <p className='text-xl text-center py-8 px-6 text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis voluptatum quaerat, beatae aliquid quia quas saepe consectetur est sapiente.</p>
-                
-                <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
-                    <div className='flex'>
-                        <div>
-                            <CheckIcon className='w-7 mr-4 text-green-600' />
-                        </div>
-                        <div>
-                            <h4 className='font-bold text-lg'>Notification</h4>
-                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
-                        </div>
-                    </div>
-                    <div className='flex'>
-                        <div>
-                            <CheckIcon className='w-7 mr-4 text-green-600' />
-                        </div>
-                        <div>
-                            <h4 className='font-bold text-lg'>Notification</h4>
-                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
-                        </div>
-                    </div>
-                    <div className='flex'>
-                        <div>
-                            <CheckIcon className='w-7 mr-4 text-green-600' />
-                        </div>
-                        <div>
-                            <h4 className='font-bold text-lg'>Notification</h4>
-                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
-                        </div>
-                    </div>
-                    <div className='flex'>
-                        <div>
-                            <CheckIcon className='w-7 mr-4 text-green-600' />
-                        </div>
-                        <div>
-                            <h4 className='font-bold text-lg'>Notification</h4>
-                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
-                        </div>
-                    </div>
-                    <div className='flex'>
-                        <div>
-                            <CheckIcon className='w-7 mr-4 text-green-600' />
-                        </div>
-                        <div>
-                            <h4 className='font-bold text-lg'>Notification</h4>
-                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
-                        </div>
-                    </div>
-                    <div className='flex'>
-                        <div>
-                            <CheckIcon className='w-7 mr-4 text-green-600' />
-                        </div>
-                        <div>
-                            <h4 className='font-bold text-lg'>Notification</h4>
-                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
-                        </div>
-                    </div>
-                    <div className='flex'>
-                        <div>
-                            <CheckIcon className='w-7 mr-4 text-green-600' />
-                        </div>
-                        <div>
-                            <h4 className='font-bold text-lg'>Notification</h4>
-                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
-                        </div>
-                    </div>
-                    <div className='flex'>
-                        <div>
-                            <CheckIcon className='w-7 mr-4 text-green-600' />
-                        </div>
-                        <div>
-                            <h4 className='font-bold text-lg'>Notification</h4>
-                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
+    <>
+            <section name="all-in-one">
+                <div className='w-full my-20'>
+                    <div className='max-w-[1240px] mx-auto px-4'>
+                        <h3 className='text-4xl font-bold text-center'>All-In-One Platform</h3>
+                        <p className='text-xl text-center py-8 px-6 text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis voluptatum quaerat, beatae aliquid quia quas saepe consectetur est sapiente.</p>
+                        
+                        <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+                            <div className='flex'>
+                                <div>
+                                    <CheckIcon className='w-7 mr-4 text-green-600' />
+                                </div>
+                                <div>
+                                    <h4 className='font-bold text-lg'>Notification</h4>
+                                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
+                                </div>
+                            </div>
+                            <div className='flex'>
+                                <div>
+                                    <CheckIcon className='w-7 mr-4 text-green-600' />
+                                </div>
+                                <div>
+                                    <h4 className='font-bold text-lg'>Notification</h4>
+                                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
+                                </div>
+                            </div>
+                            <div className='flex'>
+                                <div>
+                                    <CheckIcon className='w-7 mr-4 text-green-600' />
+                                </div>
+                                <div>
+                                    <h4 className='font-bold text-lg'>Notification</h4>
+                                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
+                                </div>
+                            </div>
+                            <div className='flex'>
+                                <div>
+                                    <CheckIcon className='w-7 mr-4 text-green-600' />
+                                </div>
+                                <div>
+                                    <h4 className='font-bold text-lg'>Notification</h4>
+                                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
+                                </div>
+                            </div>
+                            <div className='flex'>
+                                <div>
+                                    <CheckIcon className='w-7 mr-4 text-green-600' />
+                                </div>
+                                <div>
+                                    <h4 className='font-bold text-lg'>Notification</h4>
+                                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
+                                </div>
+                            </div>
+                            <div className='flex'>
+                                <div>
+                                    <CheckIcon className='w-7 mr-4 text-green-600' />
+                                </div>
+                                <div>
+                                    <h4 className='font-bold text-lg'>Notification</h4>
+                                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
+                                </div>
+                            </div>
+                            <div className='flex'>
+                                <div>
+                                    <CheckIcon className='w-7 mr-4 text-green-600' />
+                                </div>
+                                <div>
+                                    <h4 className='font-bold text-lg'>Notification</h4>
+                                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
+                                </div>
+                            </div>
+                            <div className='flex'>
+                                <div>
+                                    <CheckIcon className='w-7 mr-4 text-green-600' />
+                                </div>
+                                <div>
+                                    <h4 className='font-bold text-lg'>Notification</h4>
+                                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ipsum facere, delectus quasi explicabo ea.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
+        </section>
+    </>
   )
 }
 
